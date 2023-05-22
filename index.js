6854315854a39baf4ebad3f2f0c7b6668eb8330b
John Eric Lupisan <john.lupisan@gmail.com>
2023-05-21 19:05:51 -0800

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) 
+     return block - 42
+
+    else if (block < 42)
+     return 42 - block
+}
+
+function distanceFromHqInFeet(block) {
+    let distance = distanceFromHqInBlocks(block)
+    return (distance * 264)
+}
+
+function distanceTravelledInFeet(a, b) {
+   if (b>a) 
+    return ((b - a) * 264)
+
+   else 
+    return((a - b) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination)
+    if (feet <= 400)
+     return 0
+
+    else if (feet >= 400 && feet <= 2000)
+     return ((feet - 400) * 0.02)
+
+    else if (feet > 2000 && feet <=2500)
+     return 25
+
+    else if (feet > 2500)
+     return 'cannot travel that far'
+}
